90b52a61ebde007d8fa1482bd0c0bde4d8551957
Hassan Zahid Khan <hzk3903159@gmail.com>
2025-08-14 07:48:04 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://solvexa-tech.onrender.com/']
+# CSRF_TRUSTED_ORIGINS = ['https://solvexa-tech.onrender.com/']
 # Application definition
 
 INSTALLED_APPS = [
