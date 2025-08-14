3d477ee4875534395266a351ec71c41289901d95
Hassan Zahid Khan <hzk3903159@gmail.com>
2025-08-14 11:07:41 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -52,14 +52,15 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-       "django_browser_reload.middleware.BrowserReloadMiddleware",
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    
      
 ]
 
